717b8af1cfb84cad23235659933a1be023a47ad8
Tim Bartlett <52512775+tjb0647@users.noreply.github.com>
2022-02-03 06:47:31 -0600

    took off primeshow

--- playlist.py
+++ playlist.py
@@ -8,16 +8,17 @@ baseurl = 'http://10.0.1.7:32400'
 token = 'nYr-QVm-jNx-jBQNL_43'
 
 library = "TV Shows"
-primeshow = 'The Late Show with Stephen Colbert'
+primeshow = ' '  # 'The Late Show with Stephen Colbert'
 notwatchedplaylist = "Not.Watched.Recently.Playlist"
 playlistname = "Late Night TV"
-howmanyepisodes = [3,5] # [number of episodes when colbert is new, number of episdoes when not new]
+# [number of episodes when colbert is new, number of episdoes when not new]
+howmanyepisodes = [3, 5]
 episode_list = []
 
 # create connection to plex
 plex = PlexServer(baseurl, token)
 showlibrary = plex.library.section(library)
-#grab the prime show with the latest numbered episode
+# grab the prime show with the latest numbered episode
 
 try:
     primeshow = showlibrary.get(primeshow).episodes()[-1]
@@ -32,10 +33,10 @@ except:
 episode_list = random.sample(
     showlibrary.playlist(notwatchedplaylist).items(),
     k=howmanyepisodes[1 if not includeprime else 0]
-    )
+)
 
 if includeprime:
-    episode_list.insert(0,primeshow)
+    episode_list.insert(0, primeshow)
 
 try:
     plex.playlist(playlistname).delete()
